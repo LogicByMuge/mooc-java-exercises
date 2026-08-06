c0f164d35d1a7e92d6b0bf5df97c79bd62f45154
Juan Miguel Soriano <jmsoriano.dev@gmail.com>
2026-08-06 11:35:08 +0800

    Modified ready-made UserInterface class to show averages

--- Part 6/SeperatingUIFromProgramLogic/src/Averages/src/Main.java
+++ Part 6/SeperatingUIFromProgramLogic/src/Averages/src/Main.java
@@ -1,12 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        GradeRegister register = new GradeRegister();
-        register.addGradeBasedOnPoints(93);
-        register.addGradeBasedOnPoints(91);
-        register.addGradeBasedOnPoints(92);
+import java.util.Scanner;
 
-        System.out.println(register.averageOfPoints());
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        GradeRegister register = new GradeRegister();
+
+        UserInterface userInterface = new UserInterface(register, scanner);
+        userInterface.start();
     }
 }
