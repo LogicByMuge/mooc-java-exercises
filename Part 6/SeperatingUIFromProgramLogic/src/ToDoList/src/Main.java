8a74b3efb07bc2611dd7d5b93ea0a53e781bf702
Juan Miguel Soriano <jmsoriano.dev@gmail.com>
2026-08-06 10:27:15 +0800

    Implemented ToDoList: add,print

---
+++ Part 6/SeperatingUIFromProgramLogic/src/ToDoList/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        ToDoList list = new ToDoList();
+        list.add("read the course material");
+        list.add("watch the latest fool us");
+        list.add("take it easy");
+        list.print();
+        list.remove(2);
+        list.print();
+        list.add("buy raisins");
+        list.print();
+        list.remove(1);
+        list.remove(1);
+        list.print();
+    }
+}
