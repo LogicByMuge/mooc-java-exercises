5eec07fe70aed52decad1a400134d869d05b176b
Juan Miguel Soriano <jmsoriano.dev@gmail.com>
2026-08-21 10:36:54 +0800

    feat: Add average for passing grades

--- Part 7/LargerProgrammingExercise/src/GradeStatistics/Main.java
+++ Part 7/LargerProgrammingExercise/src/GradeStatistics/Main.java
@@ -5,12 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int sumOfPoints = 0;
         int numOfPoints = 0;
+        int sumOfPassing = 0;
+        int numOfPassing = 0;
         float average = 0;
+        float averagePassing = 0;
 
         System.out.println("Enter point totals, -1 stops:");
         while(true) {
             int point = Integer.parseInt(sc.nextLine());
-            
+
             // if input is -1 compute the average
             if(point == -1) {
                 break;
@@ -20,11 +23,16 @@ public class Main {
             if(point >= 0 && point <= 100) {
                 sumOfPoints += point;
                 numOfPoints++;
+                // Passing grade
+                if(point >= 50) {
+                    sumOfPassing += point;
+                    numOfPassing++;
+                }
             }
         }
         average = (float) sumOfPoints / numOfPoints;
-        System.out.println("Sum of points: " + sumOfPoints);
-        System.out.println("Num of points: " + numOfPoints);
+        averagePassing = (float) sumOfPassing / numOfPassing;
         System.out.println("Point average (all): " + average);
+        System.out.println("Point average (passing): " + (numOfPassing > 0 ? averagePassing : "-"));
     }
 }
